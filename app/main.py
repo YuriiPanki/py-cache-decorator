37b7f7cce3c4a54cc011868d4f42dbd16670877a
Yurii <pamkivurij92@gmail.com>
2025-11-23 13:55:46 +0100

    Corrected flaak8 error, rename wrraper -> wrapper and add the using of keyword arguments

--- app/main.py
+++ app/main.py
@@ -3,14 +3,15 @@ from typing import Callable, Any
 
 def cache(func: Callable) -> Callable:
     save_list = []
-    def wrraper(*args: Any, **kwargs: Any) -> Any:
+
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
         for item in save_list:
             if args == item[0]:
                 print("Getting from cache")
-                return item[1]
+                return item[-1]
         result = func(*args, **kwargs)
-        save_list.append([args, result])
+        save_list.append([args, kwargs, result])
         print("Calculating new result")
         return result
 
-    return wrraper
+    return wrapper
