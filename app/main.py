d9cef0f24afa08b772ce9af176423c25a9d0b1f5
Yurii <pamkivurij92@gmail.com>
2025-11-23 13:47:27 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    save_list = []
+    def wrraper(*args: Any, **kwargs: Any) -> Any:
+        for item in save_list:
+            if args == item[0]:
+                print("Getting from cache")
+                return item[1]
+        result = func(*args, **kwargs)
+        save_list.append([args, result])
+        print("Calculating new result")
+        return result
+
+    return wrraper
